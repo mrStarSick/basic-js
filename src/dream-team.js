d381a0e66e432376e161fcc415aa0c634cba0c43
mrStarSick <for_shady@mail.ru>
2021-03-14 15:32:11 +0300

    Please enter the commit message for your changes. Lines starting

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    /* throw new CustomError('Not implemented');
+    remove line with error and write your code here*/
+
+    let arr = [];
+    let members2 = [];
+    let members3 = [];
+    if (!members || arguments.length === 0) {
+        return false;
+    }
+    for (let i = 0; i < members.length; i++) {
+        if (members[i] != null) {
+            members2.push(members[i].trim().toUpperCase())
+        }
+    }
+    members2.sort();
+
+    for (let i = 0; i < members2.length; i++) {
+        if (typeof members2[i] === 'string') {
+            members3.push(members2[i])
+        }
+
+    }
+    for (let i = 0; i < members2.length; i++) {
+        arr.push(members3[i][0])
+    }
+
+    return arr.join('');
+}
